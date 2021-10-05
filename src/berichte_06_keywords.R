9cfb88fcd6fc4b3b1e9bd174267437f3a1bc5752
alanthompsonch <alan.thompson1995@gmail.com>
2021-10-05 09:34:25 +0200

    Update berichte_06_keywords.R
    Sample dingens für alle Kategorien

--- src/berichte_06_keywords.R
+++ src/berichte_06_keywords.R
@@ -58,6 +58,12 @@ sektionen_standortbestimmung <- tibble(
   )
 )
 
+#
+##
+### Wissen und Kompetenzen
+##
+#
+
 wissen <- pmap(list(berichte_cleaned_de, sektionen_standortbestimmung$x1, sektionen_standortbestimmung$x2),  function(x, start, end) {
   if(!is.na(start) & !is.na(end)) {
     x[(start+1):(end-1)]
@@ -66,6 +72,7 @@ wissen <- pmap(list(berichte_cleaned_de, sektionen_standortbestimmung$x1, sektio
     NA
   }
 })
+
 # filter NA from list
 wissen <- wissen %>%
   map(discard, is.na) %>%
@@ -92,4 +99,220 @@ wissen_clean <- map(wissen, function(doc) {
 
 wissen_clean <- str_remove_all(wissen_clean, fixed("-"))
 
+#
+##
+### Motivation und Persönlichkeit
+##
+#
+
+motper <- pmap(list(berichte_cleaned_de, sektionen_standortbestimmung$x2, sektionen_standortbestimmung$x3),  function(x, start, end) {
+  if(!is.na(start) & !is.na(end)) {
+    x[(start+1):(end-1)]
+  }
+  else {
+    NA
+  }
+})
+
+# filter NA from list
+motper <- motper %>%
+  map(discard, is.na) %>%
+  compact()
+
+# if ends on hyphen, remove hyphen and collapse. if not, collapse with space.
+source("lib/custom_flattener.R")
+motper_clean <- map(wissen, function(doc) {
+  # does the line end with a hyphen?
+  hyphen_end <- map_lgl(doc, function(line) {
+    lastchar <- str_sub(line, start = -1, end = -1) # whats the last character of the line
+    if(lastchar == "-") {
+      return(TRUE)
+    }
+    else return(FALSE)
+  })
+  out <- if(length(doc) == 1) {
+    return(doc)
+  } else {
+    return(custom_flattener(doc, hyphen_end))
+  }
+  return(out)
+})
+
+motper_clean <- str_remove_all(motper_clean, fixed("-"))
+
+#
+##
+### Umfeld
+##
+#
+
+umfeld <- pmap(list(berichte_cleaned_de, sektionen_standortbestimmung$x3, sektionen_standortbestimmung$x4),  function(x, start, end) {
+  if(!is.na(start) & !is.na(end)) {
+    x[(start+1):(end-1)]
+  }
+  else {
+    NA
+  }
+})
+
+# filter NA from list
+umfeld <- umfeld %>%
+  map(discard, is.na) %>%
+  compact()
+
+# if ends on hyphen, remove hyphen and collapse. if not, collapse with space.
+source("lib/custom_flattener.R")
+umfeld_clean <- map(umfeld, function(doc) {
+  # does the line end with a hyphen?
+  hyphen_end <- map_lgl(doc, function(line) {
+    lastchar <- str_sub(line, start = -1, end = -1) # whats the last character of the line
+    if(lastchar == "-") {
+      return(TRUE)
+    }
+    else return(FALSE)
+  })
+  out <- if(length(doc) == 1) {
+    return(doc)
+  } else {
+    return(custom_flattener(doc, hyphen_end))
+  }
+  return(out)
+})
+
+umfeld_clean <- str_remove_all(umfeld_clean, fixed("-"))
+
+#
+##
+### Unternommene Aktivitäten zur Laufbahngestaltung
+##
+#
+
+aktivitaet <- pmap(list(berichte_cleaned_de, sektionen_standortbestimmung$x4, sektionen_standortbestimmung$x5),  function(x, start, end) {
+  if(!is.na(start) & !is.na(end)) {
+    x[(start+1):(end-1)]
+  }
+  else {
+    NA
+  }
+})
+
+# filter NA from list
+aktivitaet <- aktivitaet %>%
+  map(discard, is.na) %>%
+  compact()
+
+# if ends on hyphen, remove hyphen and collapse. if not, collapse with space.
+source("lib/custom_flattener.R")
+aktivitaet_clean <- map(aktivitaet, function(doc) {
+  # does the line end with a hyphen?
+  hyphen_end <- map_lgl(doc, function(line) {
+    lastchar <- str_sub(line, start = -1, end = -1) # whats the last character of the line
+    if(lastchar == "-") {
+      return(TRUE)
+    }
+    else return(FALSE)
+  })
+  out <- if(length(doc) == 1) {
+    return(doc)
+  } else {
+    return(custom_flattener(doc, hyphen_end))
+  }
+  return(out)
+})
+
+aktivitaet_clean <- str_remove_all(aktivitaet_clean, fixed("-"))
+
+#
+##
+### Arbeitsmarkttrends
+##
+#
+
+trends <- pmap(list(berichte_cleaned_de, sektionen_standortbestimmung$x5, sektionen_standortbestimmung$x6),  function(x, start, end) {
+  if(!is.na(start) & !is.na(end)) {
+    x[(start+1):(end-1)]
+  }
+  else {
+    NA
+  }
+})
+
+# filter NA from list
+trends <- trends %>%
+  map(discard, is.na) %>%
+  compact()
+
+# if ends on hyphen, remove hyphen and collapse. if not, collapse with space.
+source("lib/custom_flattener.R")
+trends_clean <- map(trends, function(doc) {
+  # does the line end with a hyphen?
+  hyphen_end <- map_lgl(doc, function(line) {
+    lastchar <- str_sub(line, start = -1, end = -1) # whats the last character of the line
+    if(lastchar == "-") {
+      return(TRUE)
+    }
+    else return(FALSE)
+  })
+  out <- if(length(doc) == 1) {
+    return(doc)
+  } else {
+    return(custom_flattener(doc, hyphen_end))
+  }
+  return(out)
+})
+
+trends_clean <- str_remove_all(trends_clean, fixed("-"))
+
+#
+##
+### Gesundheit
+##
+#
+
+gesundh <- pmap(list(berichte_cleaned_de, sektionen_standortbestimmung$x6, sektionen_standortbestimmung$x7),  function(x, start, end) {
+  if(!is.na(start) & !is.na(end)) {
+    x[(start+1):(end-1)]
+  }
+  else {
+    NA
+  }
+})
+
+# filter NA from list
+gesundh <- gesundh %>%
+  map(discard, is.na) %>%
+  compact()
+
+# if ends on hyphen, remove hyphen and collapse. if not, collapse with space.
+source("lib/custom_flattener.R")
+gesundh_clean <- map(gesundh, function(doc) {
+  # does the line end with a hyphen?
+  hyphen_end <- map_lgl(doc, function(line) {
+    lastchar <- str_sub(line, start = -1, end = -1) # whats the last character of the line
+    if(lastchar == "-") {
+      return(TRUE)
+    }
+    else return(FALSE)
+  })
+  out <- if(length(doc) == 1) {
+    return(doc)
+  } else {
+    return(custom_flattener(doc, hyphen_end))
+  }
+  return(out)
+})
+
+gesundh_clean <- str_remove_all(gesundh_clean, fixed("-"))
+
+
 sample(wissen_clean, 1)
+
+sample(motper_clean, 1)
+
+sample(umfeld_clean, 1)
+
+sample(aktivitaet_clean, 1)
+
+sample(trends_clean, 1)
+
+sample(gesundh_clean, 1)
