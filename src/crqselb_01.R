4ec5ba066025f385cf4dd7b812c7b4d2fca692c3
alanthompsonch <alan.thompson1995@gmail.com>
2022-02-04 11:03:37 +0100

    Update crqselb_01.R
    Filter implementiert

--- src/crqselb_01.R
+++ src/crqselb_01.R
@@ -1,9 +1,11 @@
 library(haven)
 library(tidyverse)
-df_crqs <- read_sav("data/CRQ_Selbstaendige/CRQ Selbstständige_February 1, 2022_10.28.sav")
+df_crqs <- read_sav("data/CRQ_Selbststaendige/CRQ Selbstständige_February 3, 2022_12.07.sav")
 # View(df_crqs)
 
 names(df_crqs)
+
+raw_df_crqs <- df_crqs
 
 scalenames <- c("oexp", "jmk", "ssk", "inv", "con", "ccl", "jcha", "scs", "net", "cexpl", "lear")
 crq_items <- df_crqs %>% select(starts_with(scalenames)) %>% select(-consent)
@@ -14,16 +16,35 @@ scalenames <- c("jsat", "csat", "weng")
 cor_items <- df_crqs %>% select(starts_with(scalenames)) %>% select(-contains("timer"))
 names(cor_items)
 
-# careless / quality filter
+### careless / quality filter
 
-# Time filter
+## Time filter
 # timer crq + timer weng + timer csat + timer jsat > anzahl items * 2s
+zeitkriterium <- df_crqs %>% select(contains("_Page_Submit"))
+names(zeitkriterium)
+# Erstellen des Timer_totals
+zeitkriterium <- zeitkriterium %>% mutate(timer_total = crq_timer_1_Page_Submit + crq_timer_2_Page_Submit + crq_timer_3_Page_Submit + crq_timer_4_Page_Submit + crq_timer_5_Page_Submit + crq_timer_6_Page_Submit + crq_timer_7_Page_Submit + crq_timer_8_Page_Submit + crq_timer_9_Page_Submit +jsat_timer_Page_Submit + weng_timer_Page_Submit + csat_timer_Page_Submit)
+# 55 Items zwischen CRQ und Korrelaten --> 55 * 2 = 110 --> 2 Sekunden pro Item. Erstellen einer Vergleichsspalte
+zeitkriterium <- zeitkriterium %>% mutate(timer_soll = as.numeric(110))
+# Beobachtungen, die länger als 110 Sekunden hatten, werden mit TRUE markiert.
+zeitkriterium <- zeitkriterium %>% mutate(timer_test_pass = timer_soll < timer_total)
+# Integration ins main dataframe
+df_crqs <- df_crqs %>% mutate(timer_test_pass = zeitkriterium$timer_test_pass)
 
-# beide attention checks failed auch ausschluss
+
+## Attention Checks --> 0/2 = Ausschluss --> 1/2 bedeutet ein Pass
+attchks <- df_crqs %>% select(contains("attchk"))
+attchks <- attchks %>% mutate(attchk_pass = attchk_1 == 2 | attchk_2 == 5)
+df_crqs <- df_crqs %>% mutate(attchk_pass = attchks$attchk_pass)
+
+df_crqs <- df_crqs %>% filter(timer_test_pass == TRUE & attchk_pass == TRUE)
+
 
 # wenn beide filter angewendet, dann sortieren nach DE / CH
 # valide CH mind. 80
+df_crqs <- df_crqs[order(df_crqs$country_recode),]
 
+df_crqs %>% count(country_recode)
 
 # Streuung Soziodemographisch
 # Gender, Alter
